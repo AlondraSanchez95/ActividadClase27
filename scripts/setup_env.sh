87c14c450875fbc4a94b2f9e4cee81dda72cfe39
AlondraSanchez95 <al07072320@tecmilenio.mx>
2026-04-27 23:59:24 +0000

    feat: Script bash para simulacion de preparacion de entorno

---
+++ scripts/setup_env.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Bienvenido, reporte inicial del entorno" #Mensaje de bienvenida
+
+#Funcion para la instalacion de herramientas que necesitare por nombre de programa y su comando de instalacion"
+instalaciones() { 
+    PROGRAMA=$1
+    COMANDO_INSTALACION=$2
+
+    echo "Verificando $PROGRAMA..."
+    if ! command -v "$PROGRAMA" &> /dev/null
+    then
+        echo "[!] $PROGRAMA no está instalado. Instalando ahora..."
+        sudo yum update &> /dev/null
+        sudo $COMANDO_INSTALACION
+    else
+        echo "[ OK ] $PROGRAMA ya está presente en el sistema."
+    fi
+}
+
+#Llamo a la funcion dependiendo que necesite
+echo "Verificacion de herramientas"
+
+instalaciones "git" "yum install git -y"
+
+instalaciones "vim" "yum install vim -y"
+
+instalaciones "docker" "yum install docker.io -y"
+
+instalaciones "docker-compose" "yum install docker-compose"
+
+instalaciones "python3" "yum install python3"
+
+#Verifico si se tiene credenciales AWS IAM
+if aws sts get-caller-identity &> /dev/null; then
+    echo "[ OK ] AWS CLI tiene acceso (IAM Role activo)."
+else
+    echo "[!] ADVERTENCIA: AWS CLI no detecta credenciales o Rol."
+fi
+
+#Mensaje final
+echo "Final del reporte inicial del entorno"
